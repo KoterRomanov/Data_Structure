b1fbf10f478d7b8989384543175659a03f9ae372
Koter Romanov <gaole_sitc@163.com>
2019-01-08 17:22:53 +0800

    二叉查找树
    完成二叉查找树增删改查功能实现

--- BinSTree/BinSTree.cpp
+++ BinSTree/BinSTree.cpp
@@ -1,1 +1,119 @@
+#include "BinSTree.h"
 
+BinSTree::BinSTree()
+{
+  
+}
+
+BinSTree::~BinSTree()
+{
+  free( m_root );
+}
+
+//删除树
+void BinSTree::free( Node *node )
+{
+   if( NULL == node ) return;
+   
+   free( node->m_left );
+   free( node->m_right );
+   delete node;
+   node = NULL;
+}
+
+//查找键值
+int BinSTree::get( int key )
+{
+  return get( m_root, key ); 
+}
+
+//查找键值
+int BinSTree::get( Node *node, int key )
+{
+  if( node == NULL )  return NULL;
+  
+  if( key > node->m_key )       return get( node->m_right, key );
+  else if( key < node->m_key )  return get( node->m_left, key );
+  else                          return node->m_val;
+}
+
+//插入结点
+void BinSTree::put( int key, int val )
+{
+  m_root = put( m_root, key, val ); 
+}
+
+//插入结点
+Node* BinSTree::put( Node *node, int key, int val )
+{
+    if ( NULL == node ) return new Node( key, val );
+  
+    if( key > node->m_key )       node->m_right = put( node, key, val );
+    else if( key < node->m_key )  node->left = put( node, key, val );
+    else                          node->val = val;
+  
+    return node;
+}
+
+//删除结点
+void BinSTree::delet( int key )
+{
+  m_root = delet( m_root, key ); 
+}
+
+//删除结点
+Node* BinSTree::delet( Node *node, int key )
+{
+  if( NULL == node )  return NULL;
+  
+  if( key > node->m_key ) node->m_right = delet( node->m_right, key );//向右查找
+  else if( key < node->m_key ) node->m_left = delet( node->m_left, key );//向左查找
+  else {
+    //已找到结点并删除
+    //1.左、右结点为空(从逻辑上来说这行代码可省略)
+    if( NULL == node->m_left && NULL == node->m_right ) return NULL;
+    
+    //2.只有左结点
+    if( NULL == node->m_left ) {
+      Node *tmp = node->m_right;
+      delete node;
+      node = NULL;
+      return tmp;
+    }
+    
+    //3.只有右结点
+    if( NULL == node->m_right ) {
+      Node *tmp = node->m_left;
+      delete node;
+      node = NULL;
+      return tmp;
+    }
+    
+    //4.左、右结点都存在
+    Node *tmp = node;
+    node = min( tmp->m_right );//找到右子结点中最小结点
+    node->m_rigth = deletMin( tmp->m_right );//连接右结点
+    node->m_left = tmp->m_left;//连接左结点
+    delete tmp;
+    tmp = NULL;
+  }
+  
+  return node;
+}
+
+//查找最小结点
+Node* BinSTree::min( Node *node )
+{
+  if( NULL == node->m_left )  return node;
+  
+  return min( node->m_left );
+}
+
+//移除最小结点
+Node* BinSTree::deletMin( Node *node )
+{
+  if( NULL == node->m_left )  return node->m_right;
+  node->m_left = deletMin( node->m_left );
+  
+  return node;
+}
