03b959d3960c93a78fe5990894039a8d3af0901b
Koter Romanov <gaole_sitc@163.com>
2019-01-08 17:23:26 +0800

    二叉查找树
    完成二叉查找树增删改查功能定义

--- BinSTree/BinSTree.h
+++ BinSTree/BinSTree.h
@@ -1,1 +1,39 @@
+#ifndef BINSTREE_H
+#define BINSTREE_H
 
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+struct Node
+{
+  Node( int key, int val ) :m_key(key),n_val(val), m_left(NULL), m_right(NULL){}
+  
+  Node *m_left, *m_right;
+  int m_key, m_val;
+};
+
+class BinSTree
+{
+public:
+  BinSTree();
+  ~BinSTree();
+  
+  int get( int key );
+  void put( int key, int val );
+  void delet( int key );
+  
+private:
+  void free( Node *node );
+  int get( Node *node, int key );
+  Node* put( Node *node, int key, int val );
+  Node* min( Node *node );
+  Node* deletMin( Node *node );
+  
+private:
+  Node *m_root;
+  
+};
+
+#endif //BINSTREE_H
