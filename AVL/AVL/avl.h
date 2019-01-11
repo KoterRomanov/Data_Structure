b3698356689725bdebe837d6ae19e3333b2c8d14
Koter Romanov <gaole_sitc@163.com>
2019-01-11 15:52:34 +0800

    update avl.h
    定义平衡二叉树头文件

--- AVL/AVL/avl.h
+++ AVL/AVL/avl.h
@@ -14,8 +14,6 @@ using std::string;
 
 typedef int Key;
 typedef string Val;
-const bool RED = 1;
-const bool BLACK = 0;
 
 class AVLTree
 {
@@ -30,32 +28,44 @@ class AVLTree
 			n_right		= NULL;
 			n_degree	= 0;
 		}
+		
 	public:
-		Key n_key;//node key
-		Val n_val;//nodde value
-		Node *n_left, *n_right;//left and right child
-		int n_degree;//node degree
+		Key n_key;//结点的键
+		Val n_val;//结点的值
+		Node *n_left, *n_right;//左子结点、右子结点
+		int n_degree;//结点的度
 	};
 
 public:
 	AVLTree();
 	~AVLTree();
 
-	void insertNode( Key key, Val val );
-	void deleteNode( Key key );
+	void insertNode( Key key, Val val );//插入结点
+	void deleteNode( Key key );//删除结点
+	Val getNode( Key key );//获取结点的值
 	
 private:
+	Node* leftRotate( Node *node );//左旋转
+	Node* rightRotate( Node *node );//右旋转
 	Node* leftLeftRotate( Node *node );//LL
 	Node* rightRightRotate( Node *node );//RR
 	Node* leftRightRotate( Node *node );//LR
 	Node* rightLeftRotate( Node *node );//RL
+	Node* minNode( Node *node );//node子树中键最小的结点
+	Node* maxNode( Node *node );//node子树中键最大的结点
 	
-	Node *insert( Node *node, Key key, Val val );
-	Node *delet( Node *node, Key key );
+	Node *insert( Node *node, Key key, Val val );//插入新结点
+	Node *delet( Node *node, Key key );//删除键为key的结点
 	
-	void print( Node *node );//print tree
-	void free( Node *node );//free tree
+	Val getNode( Node *node, Key key );//获取结点值
+	int degree( Node *node );//计算node结点的度
+	void free( Node *node );//释放树占用内存空间
+	
+	void DLROrder( Node *node );//前序遍历
+	void LDROrder( Node *node );//中序遍历
+	void LRDOrder( Node *node );//后序遍历
+	
 private:
-	Node *m_root;//root node
+	Node *m_root;//root node//根结点
 };
 #endif //AVL_H
