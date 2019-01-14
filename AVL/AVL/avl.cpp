20c94da3c48c2a1d036cd5520547593366188f78
Koter Romanov <gaole_sitc@163.com>
2019-01-14 14:50:30 +0800

    修改BUG
    1.补充删除结点成员函数中失衡方向的判断。
    2.增加获取结点高度的成员函数。
    3.更改计算结点高度的成员函数名称。

--- AVL/AVL/avl.cpp
+++ AVL/AVL/avl.cpp
@@ -25,13 +25,20 @@ void AVLTree::free( Node *node )
 }
 
 //计算结点的高度
-int AVLTree::degree( Node *node )
+int AVLTree::calDegree( Node *node )
 {
     if( NULL == node ) return 0;
-    int leftChildDegree = degree( node->n_left );
-    int rightChildDegree = degree( node->n_right );
+    int leftChildDegree = calDegree( node->n_left );
+    int rightChildDegree = calDegree( node->n_right );
     
     return ( leftChildDegree > rightChildDegree )? (leftChildDegree+1):(rightChildDegree+1);
+}
+
+//获取结点的高度
+int AVLTree::getDegree( Node *node )
+{
+	if ( NULL == node )	return 0;
+	return node->n_degree;
 }
 
 //左旋转
@@ -41,8 +48,8 @@ AVLTree::Node* AVLTree::leftRotate( Node *node )
     node->n_right = tmp->n_left;
     tmp->n_left = node;
     
-    tmp->n_degree = degree( tmp );
-    node->n_degree = degree( node );
+    tmp->n_degree = calDegree( tmp );
+    node->n_degree = calDegree( node );
     
     return tmp;
 }
@@ -54,8 +61,8 @@ AVLTree::Node* AVLTree::rightRotate( Node *node )
     node->n_left = tmp->n_right;
     tmp->n_right = node;
     
-    tmp->n_degree = degree( tmp );
-    node->n_degree = degree( node );
+    tmp->n_degree = calDegree( tmp );
+    node->n_degree = calDegree( node );
     
     return tmp;
 }
@@ -118,10 +125,10 @@ AVLTree::Node* AVLTree::insert( Node *node, Key key, Val val )
     if( NULL == node )  return new Node( key, val );
     
     if( key < node->n_key ) {
-        //插入左子树
-        node->n_left = insert( node->n_left, key, val );
-	node->n_degree = degree( node );
-        if( degree(node->n_left) - degree(node->n_right) == 2 ) { 
+    //插入左子树
+    	node->n_left = insert( node->n_left, key, val );
+		node->n_degree = calDegree( node );//计算结点的高度
+        if( getDegree(node->n_left) - getDegree(node->n_right) == 2 ) { 
             //左树失衡
             if( key < node->n_left->n_key ) {
                 //LL
@@ -136,8 +143,8 @@ AVLTree::Node* AVLTree::insert( Node *node, Key key, Val val )
     else if( key > node->n_key ) {
         //插入右子树
         node->n_right = insert( node->n_right, key, val );
-	node->n_degree = degree( node );
-        if( degree(node->n_right) - degree(node->n_left ) == 2 ) {
+		node->n_degree = calDegree( node );//计算结点的高度
+        if( getDegree(node->n_right) - getDegree(node->n_left ) == 2 ) {
             //右树失衡
             if( key > node->n_right->n_key ) {
 		//RR
@@ -164,43 +171,53 @@ AVLTree::Node* AVLTree::delet( Node *node, Key key )
     if( key < node->n_key ) {
         //待删除结点在左子树
         node->n_left = delet( node->n_left, key );
+		node->n_degree = calDegree( node );//计算结点的高度
         //删除左子树结点后，检查右子树是否失衡
-        if( degree(node->n_right) - degree(node->n_left) == 2 ) {
-            //RR
-            node = rightRightRotate( node );
-        }
-        else {
-            //RL
-            node = rightLeftRotate( node );
+        if( getDegree(node->n_right) - getDegree(node->n_left) == 2 ) {
+			Node *rightNode = node->n_right;
+			//判断子树失衡为左还是右
+			if( getDegree(rightNode->n_left) >= get(rightNode->n_right) ) { //当左右高度相等时,按照RR情况处理
+            	//RR
+            	node = rightRightRotate( node );
+        	}
+        	else {
+            	//RL
+            	node = rightLeftRotate( node );
+			}
         }
     }
     else if( key >node->n_key ) {
         //待删除结点在右子树
         node->n_right = delet( node->n_right, key );
+		node->n_degree = calDegree( node );//计算结点的高度
         //删除右子树结点后，检查左子树是否失衡
-        if( degree(node->n_left) - degree(node->n_right) == 2 ) {
-            //LL
-            node = leftLeftRotate( node );
-        }
-        else {
-            //LR
-            node = leftRightRotate( node );
+        if( getDegree(node->n_left) - getDegree(node->n_right) == 2 ) {
+			Node *leftNode = node->n_left;
+			if( getDegree(leftNode->n_left) >= getDegree(rightNode->n_right) ) { //当左右高度相等,按照LL情况处理
+            	//LL
+            	node = leftLeftRotate( node );
+        	}
+        	else {
+            	//LR
+            	node = leftRightRotate( node );
+			}
         }
     }
-    else { //找到待删除结点
-        //1.待删除结点不存在左右子结点（则该结点为叶子结点,直接删除待删除结点）
-        //2.待删除结点存在左子结点 & 不存在右子结点（使用待删除结点的左结点替换待删除结点位置,然后删除待删除结点）
-        //3.待删除结点存在右子结点 & 不存在左子结点（使用待删除结点的右结点替换待删除结点位置,然后删除待删除结点）
-        //4.待删除结点存在左右子结点（若左子树度大于右子树,则查找左子树中键最大结点替换待删除结点位置;
-                                  //反之,则查找右子树中键最小结点替换待删除结点位置。始终删除结点多的一边）
+    else { //找到待删除结点分为4种情况
+        /* 1.待删除结点不存在左右子结点（则该结点为叶子结点,直接删除待删除结点）
+        /  2.待删除结点存在左子结点 & 不存在右子结点（使用待删除结点的左结点替换待删除结点位置,然后删除待删除结点）
+        /  3.待删除结点存在右子结点 & 不存在左子结点（使用待删除结点的右结点替换待删除结点位置,然后删除待删除结点）
+        /  4.待删除结点存在左右子结点（若左子树度大于右子树,则查找左子树中键最大结点替换待删除结点位置;
+                                  反之,则查找右子树中键最小结点替换待删除结点位置。始终删除结点多的一边） */
         if( NULL != node->n_left && NULL != node->n_right ) {
             //situation 4
-            if( node->n_left->n_degree > node->n_right->n_degree ) {
+            if( getDegree(node->n_left) > getDegree(node->n_right) ) {
                 //待删除结点左子树高于右子树
                 Node *max = maxNode( node->n_left );
                 node->n_key = max->n_key;
                 node->n_val = max->n_val;
                 node->n_left = delet( node->n_left, max->n_key );
+				node->n_degree = calDegree( node );//计算结点的高度
             }
             else {
                 //待删除结点右子树高于或等于左子树
@@ -208,6 +225,7 @@ AVLTree::Node* AVLTree::delet( Node *node, Key key )
                 node->n_key = min->n_key;
                 node->n_val = min->n_val;
                 node->n_right = delet( node->n_right, min->n_key );
+				node->n_degree = calDegree( node );//计算结点的高度
             }
         }
         else {
@@ -218,8 +236,7 @@ AVLTree::Node* AVLTree::delet( Node *node, Key key )
             tmp = NULL;
         }
     }
-    
-    node->n_degree = degree( node );//重新计算结点的度
+   
     return node;
 }
            
@@ -247,11 +264,13 @@ void AVLTree::LRDOrder( Node *node )
     cout << node->n_key << endl;
 }
 
+//根据键获取结点的值
 Val AVLTree::getNode( Key key )
 {
     return getNode( m_root, key );
 }
-           
+      
+//根据键获取结点的值
 Val AVLTree::getNode( Node *node, Key key )
 {
     if( key < node->n_key ) return getNode( node->n_left, key );
